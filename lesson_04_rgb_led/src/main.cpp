f6ca35f82a302ca46f50e6149adab43b3fbaf515
heeki <heekipark@gmail.com>
2024-10-20 16:04:36 -0400

    chore(lesson_04_rgb_led): updating tabs

--- lesson_04_rgb_led/src/main.cpp
+++ lesson_04_rgb_led/src/main.cpp
@@ -11,53 +11,53 @@ int greenValue;
 int blueValue;
 
 void setup() {
-  Serial.begin(9600);
-  Serial.println();
-  pinMode(RED, OUTPUT);
-  pinMode(GREEN, OUTPUT);
-  pinMode(BLUE, OUTPUT);
-  digitalWrite(RED, HIGH);
-  digitalWrite(GREEN, LOW);
-  digitalWrite(BLUE, LOW);
+    Serial.begin(9600);
+    Serial.println();
+    pinMode(RED, OUTPUT);
+    pinMode(GREEN, OUTPUT);
+    pinMode(BLUE, OUTPUT);
+    digitalWrite(RED, HIGH);
+    digitalWrite(GREEN, LOW);
+    digitalWrite(BLUE, LOW);
 }
 
 // main loop
 void loop() {
-  #define delayTime 10 // fading time between colors
-  char buffer[18];
+    #define delayTime 10 // fading time between colors
+    char buffer[18];
 
-  // reset values
-  redValue = 255;
-  greenValue = 0;
-  blueValue = 0;
+    // reset values
+    redValue = 255;
+    greenValue = 0;
+    blueValue = 0;
 
-  sprintf(buffer, "r=%d,g=%d,b=%d", redValue, greenValue, blueValue);
-  Serial.println(buffer);
-  for(int i = 0; i < 255; i += 1) {
-    redValue -= 1;
-    greenValue += 1;
-    analogWrite(RED, redValue);
-    analogWrite(GREEN, greenValue);
-    delay(delayTime);
-  }
+    sprintf(buffer, "r=%d,g=%d,b=%d", redValue, greenValue, blueValue);
+    Serial.println(buffer);
+    for(int i = 0; i < 255; i += 1) {
+        redValue -= 1;
+        greenValue += 1;
+        analogWrite(RED, redValue);
+        analogWrite(GREEN, greenValue);
+        delay(delayTime);
+    }
 
-  sprintf(buffer, "r=%d,g=%d,b=%d", redValue, greenValue, blueValue);
-  Serial.println(buffer);
-  for(int i = 0; i < 255; i += 1) {
-    greenValue -= 1;
-    blueValue += 1;
-    analogWrite(GREEN, greenValue);
-    analogWrite(BLUE, blueValue);
-    delay(delayTime);
-  }
+    sprintf(buffer, "r=%d,g=%d,b=%d", redValue, greenValue, blueValue);
+    Serial.println(buffer);
+    for(int i = 0; i < 255; i += 1) {
+        greenValue -= 1;
+        blueValue += 1;
+        analogWrite(GREEN, greenValue);
+        analogWrite(BLUE, blueValue);
+        delay(delayTime);
+    }
 
-  sprintf(buffer, "r=%d,g=%d,b=%d", redValue, greenValue, blueValue);
-  Serial.println(buffer);
-  for(int i = 0; i < 255; i += 1) {
-    blueValue -= 1;
-    redValue += 1;
-    analogWrite(BLUE, blueValue);
-    analogWrite(RED, redValue);
-    delay(delayTime);
-  }
+    sprintf(buffer, "r=%d,g=%d,b=%d", redValue, greenValue, blueValue);
+    Serial.println(buffer);
+    for(int i = 0; i < 255; i += 1) {
+        blueValue -= 1;
+        redValue += 1;
+        analogWrite(BLUE, blueValue);
+        analogWrite(RED, redValue);
+        delay(delayTime);
+    }
 }
