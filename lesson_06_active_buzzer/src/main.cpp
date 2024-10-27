6c70feb1b6343100105fc7daea618bda3e8dafe3
heeki <heekipark@gmail.com>
2024-10-27 16:57:18 -0400

    refactor(lesson_06_active_buzzer): updated constant name

--- lesson_06_active_buzzer/src/main.cpp
+++ lesson_06_active_buzzer/src/main.cpp
@@ -1,19 +1,19 @@
 #include <Arduino.h>
 
 // define pin constants
-#define BUZZER 12
+#define BUZZER_PIN 12
 
 void setup() {
-    pinMode(BUZZER, OUTPUT); // initialize the buzzer pin as an output
+    pinMode(BUZZER_PIN, OUTPUT); // initialize the buzzer pin as an output
 }
 
 void loop() {
     unsigned char i;
     while(1) {
         for(i=0; i<100; i++) {
-            digitalWrite(BUZZER, HIGH);
+            digitalWrite(BUZZER_PIN, HIGH);
             delay(200);
-            digitalWrite(BUZZER, LOW);
+            digitalWrite(BUZZER_PIN, LOW);
             delay(200);
         }
     }
